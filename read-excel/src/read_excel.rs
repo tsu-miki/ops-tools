1282eeb37a6729551d820a4557e7e833166b0fa8
tsu-miki <shuta.tsuzuki@uzabase.com>
2026-02-28 22:39:02 +0900

    add get global ids by revenue

---
+++ read-excel/src/read_excel.rs
@@ -0,0 +1,25 @@
+use calamine::{DataType, Error, Reader, Xlsx, open_workbook};
+
+pub fn execute(path: String) -> Result<Vec<String>, Error> {
+    let mut excel: Xlsx<_> = open_workbook(path)?;
+    let worksheet = excel.worksheet_range("企業リスト")?;
+    let rows = worksheet.rows();
+    let data_start_row = 10;
+    let global_id_col = 2;
+    let total_revenue_col = 4;
+
+    let global_ids_revenue_over_1000b: Vec<String> = rows
+        .skip(data_start_row)
+        .filter_map(|row| {
+            let revenue = row.iter().nth(total_revenue_col)?.get_float()?;
+            (revenue >= 100_000.0)
+                .then(|| row
+                    .iter()
+                    .nth(global_id_col)
+                    .map(|d| d.to_string()))
+                .flatten()
+        })
+        .collect();
+
+    Ok(global_ids_revenue_over_1000b)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use read_excel::read_excel;
+
 fn main() {
-    println!("Hello, world!");
+    let input_excel_path = format!("{}/read-excel/input/company-list.xlsx", env!("CARGO_MANIFEST_DIR"));
+    let global_ids = read_excel::execute(input_excel_path).unwrap();
+    println!("{:?}", global_ids);
 }
